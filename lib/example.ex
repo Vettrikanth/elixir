57f9726211d711b2d365ee7fcae7d73d4ea54a44
Vettrikanth <msvettrikanth@gmail.com>
2025-04-08 10:16:11 +0530

    Wrote Test cases in Function docs

--- lib/example.ex
+++ lib/example.ex
@@ -1,15 +1,21 @@
 defmodule Example do
+  @moduledoc """
+  this is for creating card
+  """
+  @doc """
+  returns a list of playing cards
+  """
   def create_deck do
     values = ["One","Two","Three","Four","Five","Six"]
     suits= ["spade","club","heart","diamond"]
 
-for value <- values, suit<-suits do
-    "#{value} of #{suit}"
+    for value <- values, suit<-suits do
+      "#{value} of #{suit}"
     end
     end
-
-
-
+  @doc """
+  it is used to shuffle
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
@@ -17,6 +23,18 @@ for value <- values, suit<-suits do
   def contains?(deck,card) do
     Enum.member?(deck,card)
   end
+
+  @doc """
+  This Is Used to split the docs based on Input Given
+
+  ## Examples
+
+        iex> deck=Example.create_deck
+        iex> {hand,deck} = Example.deal(deck,1)
+        iex>hand
+        ["One of spade"]
+
+  """
 
   def deal(deck,hand_size) do
     Enum.split(deck,hand_size)
