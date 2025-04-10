b48ff00e21402db6b4409fb6d82b9c7052bb23ff
Vettrikanth <msvettrikanth@gmail.com>
2025-04-10 14:59:05 +0530

    Installed egd dependencies

--- mix.exs
+++ mix.exs
@@ -21,6 +21,8 @@ defmodule Identicon.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:egd, "~> 0.10.1", hex: :egd24},
+      {:ex_doc, "~> 0.37.3"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
